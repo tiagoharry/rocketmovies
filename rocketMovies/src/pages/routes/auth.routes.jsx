7b4b1ab364db75181bdf6b68f9dbe3c3fd4d8eac
Tiago Assis <tiagoassisandrade@hotmail.com>
2023-06-22 13:58:46 -0300

    deploy test1

--- rocketMovies/src/pages/routes/auth.routes.jsx
+++ rocketMovies/src/pages/routes/auth.routes.jsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom'; 
+//import { Routes, Route } from 'react-router-dom'; 
 
-import { SignIn } from '../SignIn'
-import { SignUp } from '../SignUp'
+//import { SignIn } from '../SignIn'
+//import { SignUp } from '../SignUp'
 
-export function AuthRoutes() {
+/*export function AuthRoutes() {
   return (
     <Routes>
       <Route path='/' element={<SignIn />}/>
       <Route path='/register' element={<SignUp />}/>
     </Routes>
   )
-}
+}*/
